3908cd106ed2a46cc4660ccab8cb0c4683a553f1
studentvarriel <m.varriel.a.n@students.esqbs.ac.id>
2021-04-10 13:42:47 +0700

    add footer

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,6 +4,8 @@ import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
 import Categories from "parts/Categories";
 import landingPage from "json/landingPage.json";
+import Testimony from "parts/Testimony";
+import Footer from "parts/Footer";
 
 export default class LandingPage extends Component {
   constructor(props) {
@@ -21,6 +23,8 @@ export default class LandingPage extends Component {
           data={landingPage.mostPicked}
         />
         <Categories data={landingPage.categories} />
+        <Testimony data={landingPage.testimonial} />
+        <Footer />
       </>
     );
   }
